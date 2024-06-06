0b9ef7f68574afc76d8f4b7a50fcd9e0dedf4d0c
DataScientist00 <nikunjmishra00@gmail.com>
2024-06-06 09:55:19 +0530

    Create 1211. Queries Quality and Percentage.sql

---
+++ 1211. Queries Quality and Percentage.sql
@@ -0,0 +1,9 @@
+#problem link ---> https://leetcode.com/problems/queries-quality-and-percentage/description
+
+# Write your MySQL query statement below
+SELECT query_name , 
+       ROUND(AVG(rating/position),2) AS quality ,
+       ROUND(AVG(IF (rating < 3 , 1 , 0))*100,2) AS poor_query_percentage
+FROM Queries
+WHERE query_name IS NOT NULL
+GROUP BY query_name
