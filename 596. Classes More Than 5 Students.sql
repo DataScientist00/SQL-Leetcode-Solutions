74e8f2abc89c6564dad9561c7eaffc2ce9dec755
DataScientist00 <nikunjmishra00@gmail.com>
2024-06-13 09:11:05 +0530

    Create 596. Classes More Than 5 Students.sql

---
+++ 596. Classes More Than 5 Students.sql
@@ -0,0 +1,10 @@
+#problem link --->>> https://leetcode.com/problems/classes-more-than-5-students/description
+
+
+# Write your MySQL query statement below
+
+
+SELECT class
+FROM Courses
+GROUP BY class 
+HAVING COUNT(student) > 4
