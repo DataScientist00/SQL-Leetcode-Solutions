18e5d7656804917d30562375609e071aaae98178
DataScientist00 <nikunjmishra00@gmail.com>
2024-12-28 08:44:04 +0530

    Create 1693. Daily Leads and Partners.sql

---
+++ 1693. Daily Leads and Partners.sql
@@ -0,0 +1,6 @@
+#problem link-->> https://leetcode.com/problems/daily-leads-and-partners/description/
+
+# Write your MySQL query statement below
+SELECT date_id , make_name ,COUNT(DISTINCT lead_id) AS unique_leads , COUNT(DISTINCT partner_id) AS unique_partners 
+FROM DailySales
+GROUP BY date_id , make_name 
